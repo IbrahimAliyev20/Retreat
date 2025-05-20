74529f357575fba43e4d141c97cabbba8bd89782
Ibrahim_Aliyev <124527276+IbrahimAliyev20@users.noreply.github.com>
2025-05-20 15:08:16 +0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
   return (
     <div>
       <Reveal>
-      <div className="pt-34 md:pt-25 pb-10">
+      <div className="pt-34 md:pt-25">
         <HeroSection />
       </div>
       </Reveal>
