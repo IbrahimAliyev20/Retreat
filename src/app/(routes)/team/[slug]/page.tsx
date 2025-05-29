f81e2c2df731b75cc6f274c9e3c14db4967f6fd8
IbrahimAliyev20 <iboisa12@gmail.com>
2025-05-29 15:59:50 +0400

    tems 500error

--- src/app/(routes)/team/[slug]/page.tsx
+++ src/app/(routes)/team/[slug]/page.tsx
@@ -1,34 +1,37 @@
-import TeamCard from '@/components/TeamCard';
-import { getTeamBySlug, getTeams } from '@/lib/getTeamBySlug';
-import TeamSinglePage from '@/sections/(Teamsec)/TeamSinglePage';
 import { notFound } from 'next/navigation';
+import { getTeamBySlug, getTeams } from '@/lib/getTeamBySlug';
 import { TeamsType } from '@/types/type';
+import TeamSinglePage from '@/sections/(Teamsec)/TeamSinglePage';
+import TeamCard from '@/components/TeamCard';
 import Reveal from '@/components/Reveal';
 import JoinUsSection from '@/sections/JoinUsSection';
 
-export default async function TeamPage({ params }: { params: { slug: string } }) {
+export default async function TeamPage({ params }: {params:Promise<{slug:string}>}) {
   const { slug } = await params;
+
   const team = await getTeamBySlug(slug);
   const allTeams = await getTeams();
 
   if (!team) {
-    notFound(); 
+    notFound(); // 404 səhifəsinə yönləndir
   }
 
   return (
     <div className="container mx-auto py-20 flex flex-col gap-16">
       <Reveal>
-      <div>
-        <TeamSinglePage team={team} />
-      </div>
+        <div>
+          <TeamSinglePage team={team} />
+        </div>
       </Reveal>
       <div className="flex flex-col gap-10">
         <div className="flex flex-col gap-5">
           <p className="text-5xl font-semibold text-center">Digərləri ilə tanış ol</p>
-          <p className="text-lg font-semibold text-center">Sizə ən yaxşı təcrübəni təqdim etməyə sadiqik</p>
+          <p className="text-lg font-semibold text-center">
+            Sizə ən yaxşı təcrübəni təqdim etməyə sadiqik
+          </p>
         </div>
         <Reveal>
-        <TeamCard team={allTeams} />
+          <TeamCard team={allTeams} />
         </Reveal>
       </div>
       <Reveal>
@@ -40,6 +43,7 @@ export default async function TeamPage({ params }: { params: { slug: string } })
 
 export async function generateStaticParams() {
   const teams = await getTeams();
+
   return teams.map((team: TeamsType) => ({
     slug: team.slug,
   }));
